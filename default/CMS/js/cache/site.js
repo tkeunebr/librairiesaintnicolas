29b76d1d67d1dc97feb3e99cb6bab6bdfc4b91d9
qkeunebr <quentin.keunebroek@gmail.com>
2016-10-16 12:29:11 +0200

    Updated pages

---
+++ default/CMS/js/cache/site.js
@@ -0,0 +1,10 @@
+(function(){//closure
+	var app=angular.module('LibraryWebsite').config(function($translateProvider){
+	});//angular is the library, store the name of the module and the [] stands for dependencies. 
+
+	
+	app.controller('MainController',function($translate,$scope, ProjectsTitles,$location, $anchorScroll){//defines a controller.
+
+	});
+
+})();
